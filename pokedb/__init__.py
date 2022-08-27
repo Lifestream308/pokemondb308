602e2ed5012055fceab3177957cdbd6879c9411d
Lifestream308 <99936729+Lifestream308@users.noreply.github.com>
2022-08-26 19:54:18 -0700

    Added flask_cors to fix CORS problem

--- pokedb/__init__.py
+++ pokedb/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jowfndzs:s_fKMzxmDIg6D1gpNd2s_fLNlahUzEh3@heffalump.db.elephantsql.com/jowfndzs'
 # Below is Heroku database  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lldbmokvitqdzv:b907bebfd62170edc9b76cef753774986c254f2f326897b971d884346c4076a3@ec2-52-72-56-59.compute-1.amazonaws.com:5432/d5cg5ig2v1po9f'
+CORS(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 ma = Marshmallow(app)
